cbb57bb033874fe5dd5e1cf9974ad33a10bd5979
Gary Engler <gengler@InvisibrderPony.lan1>
2019-12-13 17:10:23 -0500

    Added DB Connection Check as well

--- orchestrator/app/views/main_view.py
+++ orchestrator/app/views/main_view.py
@@ -4,6 +4,7 @@ import json
 from datetime import datetime
 import redis
 from os import environ
+import psycopg2 as psql
 
 
 @app.route("/")
@@ -29,6 +30,19 @@ def main_view():
     {psql_pass}
 """)
 
+    try:
+        conn = psql.connect(
+            host=psql_host,
+            port=int(psql_port),
+            user=psql_user,
+            password=psql_pass
+        )
+
+        conn.close()
+        db_message = "Connection Made"
+    except Exception as e:
+        db_message = f"Exception: {e}"
+
     r1 = get(f"{service_01_endpoint}/heartbeat")
     if r1.status_code != 200:
         r1_resp = r1.text
@@ -53,6 +67,7 @@ def main_view():
             "timestamp": f"{datetime.now()}",
             "service_01_resp": r1_resp,
             "service_02_resp": r2_resp,
-            "redis_values": values
+            "redis_values": values,
+            "db_message": db_message
         }
     )
